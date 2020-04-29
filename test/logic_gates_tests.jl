75816f8a3213b86f785a0095bc3bd75aa4dda7da
Marco Di Tullio <marcoditullio@gmail.com>
2020-04-29 13:03:07 -0300

    added testing for hadamard gate

--- test/logic_gates_tests.jl
+++ test/logic_gates_tests.jl
@@ -6,4 +6,6 @@ using Test
     @test ucnot(Op(2),1,2)[4,3] == 1.0
     @test ucnot(Op(6),4,2)[48,64] == 1.0
     @test ucnot(Op(3),2,3)[1,2] == 0.0
+    @test hadamard(Op(3),2,1)[2,2] == 1.0
+    @test hadamard(Op(3),3,1)[5,2] == 1/sqrt(2)
 end
