56810ee2efcca81a072130ab22f28a22433502c8
Marco Di Tullio <marcoditullio@gmail.com>
2020-05-07 20:07:15 -0300

    completed tutorial, added testings

--- test/state_tests.jl
+++ test/state_tests.jl
@@ -5,6 +5,12 @@ using Test
 @testset "Fermionic.jl" begin
     @test ssp(State_sparse(spzeros(16), Op(4))) == 0
     @test ssp(State([0,0,0,1/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4))) == 1.0
+    @test ssp(State_complex([0,0,0,im/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4))) == 1.0
+    @test ssp(State_sparse_complex(1/sqrt(2)*(im*cdm(Op(4),1)*cdm(Op(4),2)+cdm(Op(4),3)*cdm(Op(4),4))*vacuum(Op(4)),Op(4))) == 1.0
+    @test rhoqsp(State_sparse(spzeros(16), Op(4)))[1,1] == 0
+    @test rhoqsp(State([0,0,0,1/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4)))[2,2] == 0.5
+    @test rhoqsp(State_complex([0,0,0,im/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4)))[1,2] == 0
+    @test rhoqsp(State_sparse_complex(1/sqrt(2)*(im*cdm(Op(4),1)*cdm(Op(4),2)+cdm(Op(4),3)*cdm(Op(4),4))*vacuum(Op(4)),Op(4)))[6,6] == 0.5
     #in order to execute these tests, enter the pkg rpl with ]
     #and execute "test PackageName"
 end
