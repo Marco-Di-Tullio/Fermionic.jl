a2fe64289fbe7176ada76f5ae2ef7595f12049f7
Marco Di Tullio <marcoditullio@gmail.com>
2021-03-31 16:45:38 -0300

    added ccd and cdc to the tutorials

--- src/Fermionic.jl
+++ src/Fermionic.jl
@@ -15,7 +15,7 @@ export Op, dim, basis, cm, cdm, cdcm, cmcd, cmcm, cdcd, vacuum
 export State, State_sparse, State_complex, State_sparse_complex, st, ope, rhosp, rhoqsp
 export eigensp, ssp, eigenqsp, sqsp, majorization_sp, majorization_qsp, n_avg, rhom, rhomnd, trp
 export sigma_x, sigma_y, sigma_z, phase, hadamard, ucnot, swap
-export fixed, basis_m, fixed_state, cdc
+export fixed, basis_m, fixed_state, cdc, ccd
 export State_fixed, State_sparse_fixed, State_complex_fixed, State_sparse_complex_fixed, nume
 export rhosp_mixed, eigensp_mixed
 end # module
