38781e6ab7db385615a7719b0be69ee0d3c2f071
Marco Di Tullio <marcoditullio@gmail.com>
2021-04-22 15:46:21 -0300

    fixed basis_m test

--- test/operator_tests.jl
+++ test/operator_tests.jl
@@ -9,7 +9,7 @@ using Test
     @test cmcm(Op(4),4,2)[9,14] == 1.0
     @test cdcd(Op(4),2,3)[15,9] == 1.0
     @test vacuum(Op(4))[1] == 1.0
-    @test basis_m(8,2)[2][4] == 9.0
+    @test basis_m(8,2)[2][4] == 10.0
     @test cdc(6,2,3,3)[6,6] == 1.0
     @test cdc(6,2,3,2)[14,15] == 1.0
     @test cdc(basis_m(4,2)[1],basis_m(4,2)[2],1,2)[4,2]==1
