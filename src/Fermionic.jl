8dc191a51a228194070f29493f9adf3c7252f936
Marco Di Tullio <marcoditullio@gmail.com>
2021-04-30 18:52:21 -0300

    New versatile structs. Starting to change complex dependancies.

--- src/Fermionic.jl
+++ src/Fermionic.jl
@@ -12,7 +12,7 @@ include("operators_fixed.jl")
 include("mixed.jl")
 
 export Op, dim, basis, cm, cdm, cdcm, cmcd, cmcm, cdcd, vacuum
-export State, State_sparse, State_complex, State_sparse_complex, st, ope, rhosp, rhoqsp
+export State, State_sparse, State_complex, State_sparse_complex, st, ope, typ, rhosp, rhoqsp
 export eigensp, ssp, eigenqsp, sqsp, majorization_sp, majorization_qsp, n_avg, rhom, rhomnd, trp
 export sigma_x, sigma_y, sigma_z, phase, hadamard, ucnot, swap
 export fixed, basis_m, fixed_state, unfixed, cdc, ccd
