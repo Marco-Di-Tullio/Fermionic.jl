6098e3c24249392984ec3519598ca7f2254f596a
Marco Di Tullio <marcoditullio@gmail.com>
2021-04-30 19:02:51 -0300

    new structs in mixed, operators_fixed and states_fixed

--- test/correlations_tests.jl
+++ test/correlations_tests.jl
@@ -5,17 +5,17 @@ using Test
 @testset "Fermionic.jl" begin
     @test ssp(State_sparse(spzeros(16), Op(4))) == 0
     @test ssp(State([0,0,0,1/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4))) == 1.0
-    @test ssp(State_complex([0,0,0,im/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4))) == 1.0
-    @test ssp(State_sparse_complex(1/sqrt(2)*(im*cdm(Op(4),1)*cdm(Op(4),2)+cdm(Op(4),3)*cdm(Op(4),4))*vacuum(Op(4)),Op(4))) == 1.0
+    @test ssp(State([0,0,0,im/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4))) == 1.0
+    @test ssp(State_sparse(1/sqrt(2)*(im*cdm(Op(4),1)*cdm(Op(4),2)+cdm(Op(4),3)*cdm(Op(4),4))*vacuum(Op(4)),Op(4))) == 1.0
     @test ssp(State_fixed(1/sqrt(2)*[1,0,0,0,0,1],Op(4),2)) == 1.0
-    @test ssp(State_complex_fixed(1/sqrt(2)*[1,0,0,0,0,im],Op(4),2)) == 1.0
+    @test ssp(State_fixed(1/sqrt(2)*[1,0,0,0,0,im],Op(4),2)) == 1.0
     @test ssp(State_fixed(1/sqrt(2)*[1,0,0,0,0,1],Op(4),2)) == 1.0
     @test ssp(State_sparse_fixed(spzeros(6),Op(4),2)) == 0
-    @test ssp(State_sparse_complex_fixed(sparse([1,6],[1,1],[1/sqrt(2),im/sqrt(2)]),Op(4),2)) == 1.0
+    @test ssp(State_sparse_fixed(sparse([1,6],[1,1],[1/sqrt(2),im/sqrt(2)]),Op(4),2)) == 1.0
     @test sqsp(State_sparse(spzeros(16), Op(4))) == 0
     @test sqsp(State([0,0,0,1/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4))) == 1.0
-    @test sqsp(State_complex([0,0,0,im/2,0,0,0,0,0,0,0,0,sqrt(3)/2,0,0,0],Op(4))) == 0.8112781244591327
-    @test sqsp(State_sparse_complex(1/sqrt(2)*(im*cdm(Op(4),1)*cdm(Op(4),2)+cdm(Op(4),3)*cdm(Op(4),4))*vacuum(Op(4)),Op(4))) == 1
+    @test sqsp(State([0,0,0,im/2,0,0,0,0,0,0,0,0,sqrt(3)/2,0,0,0],Op(4))) == 0.8112781244591327
+    @test sqsp(State_sparse(1/sqrt(2)*(im*cdm(Op(4),1)*cdm(Op(4),2)+cdm(Op(4),3)*cdm(Op(4),4))*vacuum(Op(4)),Op(4))) == 1
     @test majorization_sp(State_complex([0,0,0,im/2,0,0,0,0,0,0,0,0,sqrt(3)/2,0,0,0],Op(4)),State_complex([0,0,0,im/2,0,0,0,0,0,0,0,0,sqrt(3)/2,0,0,0],Op(4))) == 0
     @test majorization_qsp(State_complex([0,0,0,im/2,0,0,0,0,0,0,0,0,sqrt(3)/2,0,0,0],Op(4)),State_complex([0,0,0,im/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4))) == 1
     @test n_avg(State_sparse_complex(1/sqrt(2)*(im*cdm(Op(4),1)*cdm(Op(4),2)+cdm(Op(4),3)*cdm(Op(4),4))*vacuum(Op(4)),Op(4))) == 2.0
