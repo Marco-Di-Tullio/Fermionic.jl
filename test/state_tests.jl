e3a8dc984a0d6f16eaffc7fe7a405dfb4acb4833
Marco Di Tullio <marcoditullio@gmail.com>
2021-06-15 19:51:14 -0300

    added tests for precision

--- test/state_tests.jl
+++ test/state_tests.jl
@@ -7,6 +7,7 @@ using Test
     @test rhoqsp(State([0,0,0,1/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4)))[2,2] == 0.5
     @test rhoqsp(State([0,0,0,im/sqrt(2),0,0,0,0,0,0,0,0,1/sqrt(2),0,0,0],Op(4)))[1,2] == 0
     @test rhoqsp(State(1/sqrt(2)*(im*ad(Op(4),1)*ad(Op(4),2)+ad(Op(4),3)*ad(Op(4),4))*vacuum(Op(4)),Op(4)))[6,6] == 0.5
+    @test rhoqsp(State(1/sqrt(2)*(im*ad(Op(4),1)*ad(Op(4),2)+ad(Op(4),3)*ad(Op(4),4))*vacuum(Op(4)),Op(4)),4)[6,6] == 0.5
     @test non_zero([0,0,0,1,0.5], 2)[2][1] == 1.0
     @test length(non_zero([0,0,0,1,0.00005], 4)[1]) == 1
     #in order to execute these tests, enter the pkg rpl with ]
