9eda8124523834b42543c56fea1b83020e1fa15e
Marco Di Tullio <marcoditullio@gmail.com>
2021-06-22 17:26:15 -0300

    more coverage

--- test/operator_tests.jl
+++ test/operator_tests.jl
@@ -19,7 +19,6 @@ using Test
     @test aad(6,2,1,2)[8,12] == 1.0
     @test ada(basis_m(4,2)[1],basis_m(4,2)[2],3,3)[5,5] == 1
     @test fixed_state([0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],2)[6] == 1
-    @test st(fixed_state(State([0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],Op(4)),2))[6] == 1
     @test unfixed_state([0,0,0,0,1/sqrt(2),1/sqrt(2)],4,2)[13] == 1/sqrt(2)
     @test st(unfixed_state(State_fixed([0,0,0,0,0,1],Op_fixed(4,2))))[13] == 1
     @test ada(Op_fixed(4,2),1,2)[4,2] == 1
