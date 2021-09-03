4e132da2cb5e504b6155744305037ff766af0fc5
Marco Di Tullio <marcoditullio@gmail.com>
2021-09-03 14:21:14 -0300

    removed op_semifixed, added adf and af instead

--- src/Fermionic.jl
+++ src/Fermionic.jl
@@ -1,7 +1,6 @@
 module Fermionic
 using SparseArrays
 using LinearAlgebra
-using LazyStack
 
 include("base_functions.jl")
 include("operators.jl")
@@ -12,10 +11,10 @@ include("correlations.jl")
 include("logic_gates.jl")
 include("mixed.jl")
 
-export Op, dim, basis, a, ad, ada, aad, aa, adad, vacuum
+export Op, dim, basis, a, ad, ada, aad, aa, adad, adf, af, vacuum
 export fixed, basis_m,  Op_fixed
 export State, st, ope, typ, rhosp, rhoqsp, non_zero
-export State_fixed, nume, fixed_state, unfixed_state, Op_semifixed
+export State_fixed, nume, fixed_state, unfixed_state
 export eigensp, ssp, eigenqsp, sqsp, majorization_sp, majorization_qsp, n_avg, coef, rhom, rhomd, rhom2, trp
 export sigma_x, sigma_y, sigma_z, phase, hadamard, ucnot, swap
 export rhosp_mixed, eigensp_mixed
